e1823802ae4b9941960b6fb259304aba8b3f9d4c
ajya007 <ajaygubhaju223@gmail.com>
2021-08-09 19:59:51 +0545

    img-alt add

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -8,7 +8,7 @@ function Cards({el,clickHandler}) {
     return (
         <Link to="/detail" style={{ textDecoration: 'none' }}>
         <div className='cards' onClick={() => clickHandler(el.name)}>
-            <img src={el.flag}></img>
+            <img src={el.flag} alt="flag"></img>
         <h1>{el.name}</h1>
         <p><span style={{fontWeight:600}}>Population:</span> {el.population}</p>
         <p><span style={{fontWeight:600}}>Region: </span>{el.region}</p>
